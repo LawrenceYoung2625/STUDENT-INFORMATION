8fa9fecbe484657907c8ccb2d30b11f89f2363c7
LawrenceYoung2625 <92915167+LawrenceYoung2625@users.noreply.github.com>
2024-11-18 17:15:32 +0800

    Add files via upload
    umay >w<

---
+++ STUDENTinformation1/src/main/java/com/mycompany/studentinformation1/STUDENTinformation1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.studentinformation1;
+
+/**
+ *
+ * @author QCU
+ */
+public class STUDENTinformation1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
